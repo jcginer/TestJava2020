992022bfd56f67e9af7441dba0f0a51e16e1867b
jginer.it <jginer@hotebeds.com>
2023-03-25 12:04:07 +0100

    first commit (2)

---
+++ src/main/java/com/gin/testjava2020/TestJava2020Application.java
@@ -0,0 +1,13 @@
+package com.gin.testjava2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJava2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJava2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gin/testjava2020/TestJava2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gin.testjava2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJava2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
